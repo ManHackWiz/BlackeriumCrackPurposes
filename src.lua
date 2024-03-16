c3c7b7b3859964716a1fa730922a96c6f9ae8d98
MATW0lol <161067909+MATW0lol@users.noreply.github.com>
2024-03-16 21:16:35 +0000

    Update src.lua

--- src.lua
+++ src.lua
@@ -1,1 +1,16 @@
-BLACKERIUM V2 SOON
+local ellik = loadstring(game:HttpGet('https://raw.githubusercontent.com/ZepsyyCodesLUA/Ellik/main/source.lua', true))()
+
+local win = ellik:Create({
+    Title = "BlackeriumV2 Canidate"
+})
+
+local tab = win:NewTab({
+    Title = "Main"
+})
+
+tab:Button({
+    Text = 'Button',
+    Callback = function()
+        print'This might be Blackerium V2!"
+    end
+})
