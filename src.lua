972f56fae03c3002969f56b722725916ab345f9a
MATW0lol <161067909+MATW0lol@users.noreply.github.com>
2024-03-16 22:34:16 +0000

    Update src.lua

--- src.lua
+++ src.lua
@@ -1,8 +1,33 @@
-local library = loadstring(game:HttpGet('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/Moon'))() 
-local main = library:CreateWindow("Blackerium", "#19cf83", 9160626035)
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
-main:CreateTab("Home") 
+local Window = Rayfield:CreateWindow({
+   Name = "Blackerium",
+   LoadingTitle = "Blackerium V2",
+   LoadingSubtitle = "by Matwooo",
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = Blackerium,
+      FileName = "Blackerium"
+   },
+   KeySystem = true,
+   KeySettings = {
+      Title = "Blackerium V2",
+      Subtitle = "Key System",
+      Note = "Currently, you need to use a BTAK to use this Build.",
+      FileName = "BlackeriumAuthentication",
+      SaveKey = false,
+      GrabKeyFromSite = false,
+      Key = {"BTAK.KEYSYS.DEV.BLACKERIUMV2.DNL"}
+   }
+})
 
-main:CreateButton("clicky", "Home", function()
-print('blackerium lols')
-end)
+local Tab = Window:CreateTab("Main", 4483362458)
+
+local Section = Tab:CreateSection("Scripts")
+
+local Button = Tab:CreateButton({
+   Name = "Button Example",
+   Callback = function()
+print("ButtonOne TabOne has been pressed!")
+   end,
+})
